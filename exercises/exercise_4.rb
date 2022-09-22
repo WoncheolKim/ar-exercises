8ceef2ff65bc5cbdce6d7daa1e71b7851b960091
Woncheol Kim <icijiy@gmail.com>
2022-09-21 22:52:15 -0700

    exercises04

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch (a collection of) only those stores that carry men's apparel.
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |mens_store|
+  puts "Name: #{mens_store.name}, Annual Revenue: $#{mens_store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores_revenue_under_one_million= @womens_stores.where('annual_revenue < ?', 1000000)
+@womens_stores_revenue_under_one_million.each do |ws|
+  puts ws.name
+end
